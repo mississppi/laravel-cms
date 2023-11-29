8c92ffbb267df94f07b275b0b0b89d665a2d20ca
mississppi <msspstudio@gmail.com>
2023-11-29 20:18:58 +0900

    add controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|unique:categories|max:255',
+        ]);
+
+        Category::create([
+            'title' => $request->input('title'),
+        ]);
+
+        return redirect()->route('categories.index')->with('success', 'Category created successfully');
+    }
+
+    public function edit(Category $category)
+    {
+        return view('categories.edit', compact('category'));
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $request->validate(
+            ['title' => 'required|unique:categories|max:255'],
+        );
+
+        $category->update([
+            'title' => $request->input('title'),
+        ]);
+
+        return redirect()->route('categories.index')->with('success', 'Category updated successfully');
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('categories.index')->with('success', 'Category deleted successfully');
+    }
+}
